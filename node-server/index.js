1420fbd8b9b6048fc7ae060daceb4c3003c56c62
Md Serif <mdserif1998@gmail.com>
2025-02-08 19:56:02 +0530

    Learned about HTTP Methods

--- node-server/index.js
+++ node-server/index.js
@@ -7,7 +7,7 @@ const myServer = http.createServer((req,res)=>{
     if(req.url === "/favicon.ico"){return res.end()}
     const myUrl=url.parse(req.url,true)
     console.log(myUrl)
-    const log = `${Date.now()} : ${req.url} : New Request Received\n`;
+    const log = `${Date.now()} : ${req.method} : ${req.url} : New Request Received\n`;
     // console.log(req.headers)
     fs.appendFile("log.txt",log,(err,data)=>{
         switch(myUrl.pathname){
@@ -16,7 +16,14 @@ const myServer = http.createServer((req,res)=>{
             case "/about" :
                 const name = myUrl.query.myname;
                 res.end(`hi my name is ${name}`)
-            break;
+                break;
+            case "/signup":
+                if(req.method === "GET"){
+                    res.end("this is a signup form")
+                }else if(req.method === "POST"){
+                    // DB Query
+                    res.end("success")
+                }
             default : res.end("404 Not found");
         }       
     })
