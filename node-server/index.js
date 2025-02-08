66e2deb2e27e1072055d11828639c4d1443f7448
Md Serif <mdserif1998@gmail.com>
2025-02-08 19:35:09 +0530

    Handeling urls in nodejs

--- node-server/index.js
+++ node-server/index.js
@@ -1,14 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 
 const myServer = http.createServer((req,res)=>{
+    if(req.url === "/favicon.ico"){return res.end()}
+    const myUrl=url.parse(req.url,true)
+    console.log(myUrl)
     const log = `${Date.now()} : ${req.url} : New Request Received\n`;
     // console.log(req.headers)
     fs.appendFile("log.txt",log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/" : res.end("Home page");
             break;
-            case "/about" : res.end("About page")
+            case "/about" :
+                const name = myUrl.query.myname;
+                res.end(`hi my name is ${name}`)
             break;
             default : res.end("404 Not found");
         }       
