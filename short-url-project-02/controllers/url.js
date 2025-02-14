d1759809d42daa4b21e6c71e430e6c752efc36e2
Md Serif <mdserif1998@gmail.com>
2025-02-14 12:21:52 +0530

    home page frontend part completed

--- short-url-project-02/controllers/url.js
+++ short-url-project-02/controllers/url.js
@@ -3,14 +3,15 @@ const shortid = require('shortid');
 
 const handleGenerateNewShortURL = async (req,res) => {
     const body = req.body;
-    if(!body.url){return res.status(400).json({error:"url is required"});
+    if(!body.url){
+        return res.json({error: "url is required"});
+        // return res.redirect("/")
     }
     const shortID = shortid();
     await URL.create({shortId:shortID,redirectURL:body.url,visitHistory:[]
     })
 
     return res.render("home",{id:shortID})
-    return res.json({id:shortID})
 }
 
 const handleUpdateVisitHistoryAndRedirect = async (req,res)=>{
